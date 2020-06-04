463e523d208bfb71c281b55c94f2bd6bf9524b49
tayflo <54722159+Tayflo@users.noreply.github.com>
2020-06-04 16:35:54 +0200

    fix unecessary indent in comment

--- util/matrice.js
+++ util/matrice.js
@@ -128,16 +128,16 @@ function subtractMatrice(m1, m2) {
 
 /* OKAOU // https://www.quora.com/What-is-the-code-to-develop-a-script-to-multiply-two-matrices-using-HTML-and-JavaScript
 function multiplyMatrix(m1, m2) {
-    var result = [];
-       for(var j = 0; j < m2.length; j++) {
-        result[j] = [];
-        for(var k = 0; k < m1[0].length; k++) {
-            var sum = 0;
-            for(var i = 0; i < m1.length; i++) {
-                sum += m1[i][k] * m2[j][i];
-            }
-            result[j].push(sum);
-        }
+  var result = [];
+    for(var j = 0; j < m2.length; j++) {
+    result[j] = [];
+    for(var k = 0; k < m1[0].length; k++) {
+      var sum = 0;
+      for(var i = 0; i < m1.length; i++) {
+        sum += m1[i][k] * m2[j][i];
+      }
+      result[j].push(sum);
     }
-    return result;
+  }
+  return result;
 }*/
